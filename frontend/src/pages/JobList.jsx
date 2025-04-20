be1e2c6174a4461c603ba84c2b5d2f08d74d347c
Subham <mr.subhamkumarsingh@gmail.com>
2025-04-21 02:02:25 +0530

    deploy

--- frontend/src/pages/JobList.jsx
+++ frontend/src/pages/JobList.jsx
@@ -1,9 +1,10 @@
 import JobCard from "../components/JobCard";
 
 export default function JobList({ jobs }) {
-  return (
+ const reversejobs=jobs.reverse();
+    return (
     <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-4 items-center justify-center">
-      {jobs.map((job) => (
+      {reversejobs.map((job) => (
         <JobCard key={job._id} job={job} />
       ))}
     </div>
